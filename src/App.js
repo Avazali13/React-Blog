2db7c1a2f6eb3c732bbe0c6618c2cd37baa5773e
example <email@example.com>
2023-02-02 23:41:49 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import {Navbar} from "./components/Navbar"
-import {Main} from "./components/Main"
+import {Main, Main2,Main3} from "./components/Main"
 import  "./style.css";
 
+
 function App() {
+  
 return (
   <>
   <Navbar class='Navbar'/>
   <Main class='main'/>
+  <Navbar class='Navbar'/>
+<Main2 class='main2'></Main2>
+<Navbar class='Navbar'/>
+<Main3 class="main3"></Main3>
 
   </>
 )
