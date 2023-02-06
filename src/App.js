3f3998c6eb69c0a145185870480fc8f8131224a8
example <email@example.com>
2023-02-06 08:43:25 +0400

    seccc

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import {Navbar} from "./components/Navbar"
-import {Main, Main2,Main3} from "./components/Main"
+import Home from "./components/Home"
+import About from "./components/About"
+import Contact from "./components/Contact"
 import  "./style.css";
+import Portofolios from "./components/Portofolios";
+
 
 
 function App() {
   
 return (
   <>
-  <Navbar class='Navbar'/>
-  <Main class='main'/>
-  <Navbar class='Navbar'/>
+<Home></Home>
+<About></About>
+<Contact></Contact>
+<Portofolios/>
+
+
+
+  {/* <Navbar class='Navbar'/>
+  <Main class='main'/> */}
+  {/* <Navbar class='Navbar'/>
 <Main2 class='main2'></Main2>
 <Navbar class='Navbar'/>
-<Main3 class="main3"></Main3>
+<Main3 class="main3"></Main3> */}
 
+{/* <Portofilo></Portofilo> */}
   </>
 )
  }
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import { Navbar } from "./Navbar";
+import {Main} from "./Main"
+
+
+
+
+function Home (){
+    return(
+<div>
+<Navbar className="Navbar"/>
+<Main class="main" ></Main>
+</div>
+    )
+}
+export default Home;
